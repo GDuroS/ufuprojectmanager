73cf9c591a90dadf57e70b57d1a77ac06b376d28
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2024-03-27 17:17:47 +0000

    Edited MenuBuildUtils and theme/parameters.yaml

--- client_code/MenuBuildUtils.py
+++ client_code/MenuBuildUtils.py
@@ -4,8 +4,10 @@ from OruData.Model.Menu import Menu
 MENUS = []
 
 CADASTRAR_USER_STORY = Menu(key="CADASTRAR_USER_STORY", name="User Stories", secure=True, icon="fa:file-signature", action="route", route={'url_hash': 'story'})
+CADASTRAR_TASK = Menu(key="CADASTRAR_TASK", name="Tarefas", secure=True, icon="fa:list-check", action="route", route={'url_hash': 'task'})
 CADASTRAR_ITERACAO = Menu(key="CADASTRAR_ITERACAO", name="Sprints", secure=True, icon="fa:person-chalkboard", action="route", route={'url_hash': 'sprint'})
-CADASTRAR_RELEASE = Menu(key="CADASTRAR_RELEASE", name="Releases", secure=True, icon="fa:person-chalkboard", action="route", route={'url_hash': 'releases'})
+CADASTRAR_RELEASE = Menu(key="CADASTRAR_RELEASE", name="Releases", secure=True, icon="fa:code-merge", action="route", route={'url_hash': 'releases'})
+SCRUM_BOARD = Menu(key="SCRUM_BOARD", name="Scrum Board", secure=False, icon="fa:file-signature", action="route", route={'url_hash': 'story'})
 
 def get_menu():
   return [
