2914639e881c2971dddaa7c89ab9960688c54d1b
gabriel.duro.s@gmail.com <gabriel.duro.s@gmail.com>
2024-04-03 03:36:21 +0000

    Edited theme.css, Views.UserStories.UserStoryCadastro and Resource.UserStoryResource

--- server_code/Resource/UserStoryResource.py
+++ server_code/Resource/UserStoryResource.py
@@ -12,14 +12,16 @@ def getUserStoryById(sequence:int):
 def getUserStoriesFind(*args, **kwargs):
   return AbstractService.find(TABLE, *args, **kwargs)
 
-@anvil.server.callable
+@anvil.server.callable(require_user=True)
 def postUserStory(user_story):
+  from anvil import users
+  user_story['Criador'] = users.get_user()
   return AbstractService.save(TABLE, user_story, True)
 
-@anvil.server.callable
+@anvil.server.callable(require_user=True)
 def putUserStory(row, user_story):
   return AbstractService.update(row, user_story)
 
-@anvil.server.callable
+@anvil.server.callable(require_user=True)
 def deleteUserStory(user_story):
   return AbstractService.delete(user_story)
